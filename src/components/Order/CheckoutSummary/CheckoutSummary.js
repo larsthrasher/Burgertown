ff658545a01852b2a68f551a4bfc234889cbc35e
larsthrasher <thrasherlars@gmail.com>
2020-07-03 14:39:48 -0400

    checkout to home page route push

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -9,7 +9,7 @@ import classes from './CheckoutSummary.css';
 const checkoutSummary = (props) => {
   return (
     <div className={classes.CheckoutSummary}>
-      <h1>Hope it's tastin' good.</h1>
+      <h1>Hope it's tasting good.</h1>
       <div style={{width: '100%', height: '300px', margin: 'auto'}}>
           <Burger ingredients={props.ingredients}/>
       </div>
